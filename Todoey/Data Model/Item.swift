828040c56bc04fd19c596ea46a09be435da42e50
Manuel Soto <sotomanuel41@icloud.com>
2018-07-28 13:09:31 -0500

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Manuel Soto on 7/28/18.
+//  Copyright © 2018 Manuel Soto. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done :Bool = false
+}
